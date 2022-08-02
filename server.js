92260c4eb4d8cf986dbd769d9c93b0415de25899
vihith arekatla <arekatlavihith@gmail.com>
2022-08-02 15:52:58 +0530

    make it better

--- server.js
+++ server.js
@@ -238,9 +238,10 @@ app.get("/chats",async (req,resp)=>{
         { $and: [{person1: req.query.recv}, {person2: req.query.send}] }
     ]});
     let val1=await Contact.find({user_name:req.query.send});
+    console.log("hi");
+    console.log(val);
     if(val.length!==0){
         val[0].chats.sort(GetSortOrder("date_act"));
-        console.log(val[0].chats);
         if(val1.length!==0){
             resp.render("chat_ejs.ejs",{ people:val1[0].user_contacts,selected_user:req.query.recv,messages:val[0].chats,send:req.query.send,recv:req.query.recv});
         }
